b5a9dd5946482d2f32971ff942ee72a44e3867cd
Akihiro Tojo <akihiro.021109@gmail.com>
2024-07-06 00:19:13 +0900

    Update map to render with props

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -5,7 +5,7 @@ import { TileLayer } from '@deck.gl/geo-layers';
 import { GeoJsonLayer } from '@deck.gl/layers';
 import DeckGL from '@deck.gl/react';
 import { PMTilesSource } from '@loaders.gl/pmtiles';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 
 const INITIAL_VIEW_STATE = {
   longitude: -3.7492,
@@ -15,7 +15,12 @@ const INITIAL_VIEW_STATE = {
   bearing: 0,
 };
 
-const tileSource = new PMTilesSource({
+const ZOOM_SETTINGS = {
+  minZoom: 0,
+  maxZoom: 5,
+};
+
+const basemapTileSource = new PMTilesSource({
   // TODO: Replace url
   url: 'http://localhost:2999/ne_110m_land.pmtiles',
 });
@@ -27,31 +32,76 @@ type BBox = {
   north: number;
 };
 
-export const Map: FC = () => {
-  const tileLayer = new TileLayer({
-    getTileData: tileSource.getTileData,
-    minZoom: 0,
-    maxZoom: 5,
-    renderSubLayers: ({ id, data, tile }) => {
-      const bbox = tile.bbox as BBox;
-      return new GeoJsonLayer({
-        id: `geojson-layer-${id}`,
-        data,
-        extensions: [new ClipExtension()],
-        clipBounds: [bbox.west, bbox.south, bbox.east, bbox.north],
-        lineWidthScale: 1,
-        lineWidthMinPixels: 2,
-        getLineColor: [200, 200, 200],
-        filled: false,
-      });
-    },
-  });
+type GeoJsonFeature = {
+  id: string;
+};
+
+export interface MapProps {
+  year: string;
+  focusId: string;
+}
+
+export const Map: FC<MapProps> = ({ year, focusId }) => {
+  const basemapLayer = useMemo(() => {
+    return new TileLayer({
+      id: 'basemap-layer',
+      ...ZOOM_SETTINGS,
+      getTileData: basemapTileSource.getTileData,
+      renderSubLayers: ({ data, tile }) => {
+        const bbox = tile.bbox as BBox;
+        return new GeoJsonLayer({
+          id: `basemap-layer--${tile.id}`,
+          data,
+          extensions: [new ClipExtension()],
+          clipBounds: [bbox.west, bbox.south, bbox.east, bbox.north],
+          lineWidthScale: 1,
+          lineWidthMinPixels: 2,
+          getLineColor: [200, 200, 200],
+          filled: false,
+        });
+      },
+    });
+  }, []);
+
+  const featureTileSource = useMemo(() => {
+    return new PMTilesSource({
+      // TODO: Replace url
+      url: `http://localhost:2999/${year}.pmtiles`,
+    });
+  }, [year]);
+
+  const featureLayer = useMemo(() => {
+    return new TileLayer({
+      id: 'feature-layer',
+      ...ZOOM_SETTINGS,
+      getTileData: featureTileSource.getTileData,
+      renderSubLayers: ({ data, tile }) => {
+        const bbox = tile.bbox as BBox;
+        return new GeoJsonLayer<GeoJsonFeature, { clipBounds: number[] }>({
+          id: `feature-layer--${tile.id}`,
+          data,
+          extensions: [new ClipExtension()],
+          clipBounds: [bbox.west, bbox.south, bbox.east, bbox.north],
+          lineWidthScale: 1,
+          lineWidthMinPixels: 2,
+          getLineColor: (d) =>
+            d.properties.id === focusId
+              ? [200, 100, 100, 200]
+              : [200, 100, 100, 50],
+          getFillColor: (d) =>
+            d.properties.id === focusId
+              ? [200, 100, 100, 100]
+              : [200, 100, 100, 25],
+        });
+      },
+    });
+  }, [featureTileSource, focusId]);
 
   return (
     <DeckGL
       initialViewState={INITIAL_VIEW_STATE}
       controller
-      layers={[tileLayer]}
+      layers={[basemapLayer, featureLayer]}
     />
   );
 };
