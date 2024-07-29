88372164a5f2af3a56c6b7a0f849820733541ca7
Akihiro Tojo <akihiro.tj.biz+github@gmail.com>
2024-07-29 21:04:35 +0900

    Update article page style (#28)
    * Update article page dom structure
    
    * Update article page color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body
+        className="bg-slate-950 text-white"
         style={{
           fontFamily: [
             inter.style.fontFamily,
