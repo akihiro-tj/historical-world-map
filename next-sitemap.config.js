65a5112b33098d40a2620ba5d513f73402f82783
Akihiro Tojo <akihiro.021109@gmail.com>
2024-07-30 20:10:56 +0900

    Add script to generate sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  // TODO: https://github.com/akihiro-tj/historical-world-map/issues/38
+  siteUrl: 'https://example.com',
+  generateRobotsTxt: true,
+};
